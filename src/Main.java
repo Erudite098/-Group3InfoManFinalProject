8812b77b8d6e763f8645e99f997d00646a8782ff
happypoot300 <happypoot300@gmail.com>
2024-06-17 19:58:46 +0800

    hehehehe

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
-import javax.swing.JFrame;
-
 public class Main {
 
 	public static void main(String[] args) {
-		
-		loginPage login = new loginPage();		
+
+		loginPage login = new loginPage();
 		login.start();
-		
+
 		System.out.println("Hello Word");
+
+		System.out.println("hii~");
 	}
 
 }
